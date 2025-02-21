c795705d3c10b6a247c652da16945491d14413cb
kanutten <44906532+kanutten@users.noreply.github.com>
2025-02-21 04:42:01 +0100

    add fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    atuin init fish | source
+    zoxide init fish | source
+    starship init fish | source
+    # Commands to run in interactive sessions can go here
+end
